f77260dbcd0145507dc51e5c4ae803a8967f6e12
bergconnor <cberg1@ksu.edu>
2017-04-20 13:47:03 -0500

    added more base files

--- server.js
+++ server.js
@@ -2,9 +2,41 @@
  * Server for a reddit-like web app.
  */
 
-// Constants
-const PORT = 3000;
+"use strict";
 
-// Requires
-var fs = require('fs');
+// constants
+var PORT = 3000;
+
+// requires
 var http = require('http');
+var fileserver = require('./lib/fileserver');
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('reddit.sqlite3', function(err) {
+  if(err) console.error(err);
+});
+var router = new (require('./lib/route')).Router(db);
+
+// Cache static directory in the fileserver
+fileserver.loadDir('public');
+
+// Define our routes
+var project = require('./src/resource/project');
+router.resource('/projects', project);
+
+var server = new http.Server(function(req, res) {
+  // Remove the leading '/' from the resource url
+  var resource = req.url.slice(1);
+  // If no resource is requested, serve the cached index page.
+  if(resource == '')
+    fileserver.serveFile('public/index.html', req, res);
+  // If the resource is cached in the fileserver, serve it
+  else if(fileserver.isCached(resource))
+    fileserver.serveFile(resource, req, res);
+  // Otherwise, route the request
+  else router.route(req, res);
+});
+
+// Launch the server
+server.listen(PORT, function(){
+  console.log("listening on port " + PORT);
+});
